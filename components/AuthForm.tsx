b8f8c8663fe2059f01df5f32bdb6b4b878c001fc
fasdjkherig <farahat.farahat14141679@nursing.menofia.edu.eg>
2024-06-17 19:19:44 +0300

    complete the design of auth page and connect to appwrite

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -18,46 +18,107 @@ import {
 import { Input } from "@/components/ui/input";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
-// import { getLoggedInUser, signIn, signUp } from "@/lib/actions/user.actions";
-import PlaidLink from "./PlaidLink";
+import { getLoggedInUser, signIn, signUp } from "@/lib/actions/user.actions";
+// import PlaidLink from "./PlaidLink";
 
-const formSchema = z.object({
-  email: z.string().email(),
-  password: z
-    .string()
-    .min(8, "Password must be at least 8 characters long")
-    .max(100, "Password must be at most 100 characters long")
-    .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
-    .regex(/[a-z]/, "Password must contain at least one lowercase letter")
-    .regex(/[0-9]/, "Password must contain at least one number")
-    .regex(
-      /[^A-Za-z0-9]/,
-      "Password must contain at least one special character"
-    ),
-});
+// formSchema
+const authFormSchema = (type: string) =>
+  z.object({
+    // sign-up
+    firstname:
+      type === "sign-in"
+        ? z.string().optional()
+        : z.string().min(1, "First name is required"),
+    lastname:
+      type === "sign-in"
+        ? z.string().optional()
+        : z.string().min(1, "Last name is required"),
+    address:
+      type === "sign-in"
+        ? z.string().optional()
+        : z.string().min(1, "Address is required"),
+    city:
+      type === "sign-in"
+        ? z.string().optional()
+        : z.string().min(1, "Address is required"),
+    state:
+      type === "sign-in"
+        ? z.string().optional()
+        : z.string().min(1, "State is required"),
+    postalcode:
+      type === "sign-in"
+        ? z.string().optional()
+        : z.string().regex(/^[0-9]{5}$/, "Invalid postal code format"),
+    dateofbirth:
+      type === "sign-in"
+        ? z.string().optional()
+        : z
+            .string()
+            .regex(/^\d{4}-\d{2}-\d{2}$/, "Invalid date format (YYYY-MM-DD)"),
+    ssn:
+      type === "sign-in"
+        ? z.string().optional()
+        : z
+            .string()
+            .regex(/^\d{3}-\d{2}-\d{4}$/, "Invalid SSN format (XXX-XX-XXXX)"),
+    // sign-in
+    email: z.string().email("Invalid email format"),
+    password: z.string().min(6, "Password must be at least 6 characters long"),
+  });
 
 const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter();
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  //   // 1. Define your form
+  const formSchema = authFormSchema(type);
+
+  // 1. Define your form
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      email: "",
-      password: "",
-    },
+    defaultValues: { email: "", password: "" },
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
     setIsLoading(true);
 
-    console.log(values);
+    try {
+      // Sign up with Appwrite & create plaid token
 
-    setIsLoading(false);
-  }
+      if (type === "sign-up") {
+        const userData = {
+          firstName: data.firstname!,
+          lastName: data.lastname!,
+          address: data.address!,
+          city: data.city!,
+          state: data.state!,
+          postalcode: data.postalcode!,
+          dateofbirth: data.dateofbirth!,
+          ssn: data.ssn!,
+          email: data.email,
+          password: data.password,
+        };
+
+        const newUser = await signUp(userData);
+
+        setUser(newUser);
+      }
+
+      if (type === "sign-in") {
+        const response = await signIn({
+          email: data.email,
+          password: data.password,
+        });
+
+        if (response) router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <section className="auth-form">
@@ -87,12 +148,165 @@ const AuthForm = ({ type }: { type: string }) => {
       </header>
       {user ? (
         <div className="flex flex-col gap-4">
-          <PlaidLink user={user} variant="primary" />
+          {/* <PlaidLink user={user} variant="primary" /> */}
         </div>
       ) : (
         <>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              {type === "sign-up" && (
+                <>
+                  <div className="flex gap-4">
+                    <FormField
+                      control={form.control}
+                      name="firstname"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>First Name</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="John"
+                              className="input-class"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage className="form-message mt-2" />
+                        </FormItem>
+                      )}
+                    />
+
+                    <FormField
+                      control={form.control}
+                      name="lastname"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Last Name</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="ex: Doe"
+                              className="input-class"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage className="form-message mt-2" />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+
+                  <FormField
+                    control={form.control}
+                    name="address"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Address</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Enter your specific address"
+                            className="input-class"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage className="form-message mt-2" />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="city"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>City</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Enter your city"
+                            className="input-class"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage className="form-message mt-2" />
+                      </FormItem>
+                    )}
+                  />
+
+                  <div className="flex gap-4">
+                    <FormField
+                      control={form.control}
+                      name="state"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>State</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="ex: NY"
+                              className="input-class"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage className="form-message mt-2" />
+                        </FormItem>
+                      )}
+                    />
+
+                    <FormField
+                      control={form.control}
+                      name="postalcode"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Postal Code</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="ex: 11101"
+                              className="input-class"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage className="form-message mt-2" />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+
+                  <div className="flex gap-4">
+                    <FormField
+                      control={form.control}
+                      name="dateofbirth"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Date of Birth</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="yyyy-mm-dd"
+                              className="input-class"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage className="form-message mt-2" />
+                        </FormItem>
+                      )}
+                    />
+
+                    <FormField
+                      control={form.control}
+                      name="ssn"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>SSN</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="ex: 1234"
+                              className="input-class"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage className="form-message mt-2" />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                </>
+              )}
               <FormField
                 control={form.control}
                 name="email"
@@ -129,20 +343,36 @@ const AuthForm = ({ type }: { type: string }) => {
                   </FormItem>
                 )}
               />
-              <Button type="submit" disabled={isLoading} className="form-btn">
-                {isLoading ? (
-                  <>
-                    <Loader2 size={20} className="animate-spin" /> &nbsp;
-                    Loading...
-                  </>
-                ) : type === "sign-in" ? (
-                  "Sign In"
-                ) : (
-                  "Sign Up"
-                )}
-              </Button>
+              <div className="flex flex-col gap-4">
+                <Button type="submit" disabled={isLoading} className="form-btn">
+                  {isLoading ? (
+                    <>
+                      <Loader2 size={20} className="animate-spin" /> &nbsp;
+                      Loading...
+                    </>
+                  ) : type === "sign-in" ? (
+                    "Sign In"
+                  ) : (
+                    "Sign Up"
+                  )}
+                </Button>
+              </div>
             </form>
           </Form>
+
+          <footer className="flex justify-center gap-1">
+            <p className="text-14 font-normal text-gray-600">
+              {type === "sign-in"
+                ? "Don't have an account?"
+                : "Already have an account?"}
+            </p>
+            <Link
+              href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+              className="form-link"
+            >
+              {type === "sign-in" ? "Sign up" : "Sign in"}
+            </Link>
+          </footer>
         </>
       )}
     </section>
---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,5 @@
+"use server";
+
+export const signIn = async ({ email, password }: signInProps) => {};
+
+export const signUp = async ({ password, ...userData }: SignUpParams) => {};
